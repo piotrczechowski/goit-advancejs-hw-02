ab4921be1bdd6700afea34c344dd2ebbb4c2b04d
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2024-12-22 23:58:04 +0100

    update structure

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   plugins: [ghPages()],
-  base: 'goit-advancejs-hw-02', 
+  base: 'https://github.com/piotrczechowski/goit-advancejs-hw-02', 
+
+  build: {
+    outDir: 'dist', // Directory where the build files are generated
+    assetsDir: 'assets', // Directory for static assets
+    rollupOptions: {
+      input: {
+        main: 'index.html', // Entry point of the application
+      },
+    },
+  },
+
 });
