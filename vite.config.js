46a0649d373484a6ed30a3eeb38acfe0ddd055a7
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2024-12-23 00:14:55 +0100

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/goit-advancejs-hw-02/', // Replace with your GitHub repository name
   build: {
+    rollupOptions: {
+        input: {
+          main: 'index.html', // Main entry point
+          
+        },
+      },
     outDir: 'dist',
     assetsDir: 'assets',
   },
